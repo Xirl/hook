737133389d7c9f0eedba094539294d7d164f9bd9
细雪 <1981486693@qq.com>
2022-10-25 15:52:48 +0800

    a

--- back.sh
+++ back.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-WORK_PATH='/usr/projects/vue-back'
+WORK_PATH='/usr/projects/back'
 cd $WORK_PATH
 echo "清理代码"
-git reset --hard origin/master
+git reset --hard origin/main
 git clean -f
 echo "拉取最新代码"
-git pull origin master
+git pull origin main
 echo "开始构建镜像"
-docker build -t vue-back .
+docker build -t back .
 echo "删除旧容器"
-docker stop vue-back-container
-docker rm vue-back-container
+docker stop back-container
+docker rm back-container
 echo "启动新容器"
-docker container run -p 3000:3000 -d --name vue-back-container vue-back
+docker container run -p 3000:3000 -d --name back-container back
--- front.sh
+++ front.sh
@@ -2,10 +2,10 @@
 WORK_PATH='/usr/projects/front'
 cd $WORK_PATH
 echo "清理代码"
-git reset --hard origin/master
+git reset --hard origin/main
 git clean -f
 echo "拉取最新代码"
-git pull origin master
+git pull origin main
 echo "打包最新代码"
 npm run build
 echo "开始构建镜像"
