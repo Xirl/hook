9d53bfa29cf0a0e4a2b296b9bb31f3409c69fc8d
细雪 <1981486693@qq.com>
2022-10-25 14:19:47 +0800

    aa

---
+++ hook.js
@@ -0,0 +1,33 @@
+let http = require('http');
+let crypto = require('crypto');
+var spawn = require('child_process').spawn;
+const SECRET = '123456';
+function sign (data) {
+  return 'sha1=' + crypto.createHmac('sha1', SECRET).update(data).digest('hex')
+}
+let server = http.createServer(function(req,res){
+  console.log(req.method,req.url);
+  if(req.url == '/hook' && req.method =='POST'){
+    let buffers = []; 
+    req.on('data',function(data){
+      buffers.push(data);
+    });
+    req.on('end',function(){
+      let body = Buffer.concat(buffers);
+      let sig   = req.headers['x-hub-signature'];
+      let event = req.headers['x-github-event'];
+      let id    = req.headers['x-github-delivery'];
+      if(sig !== sign(body)){
+        return res.end('Not Allowed');
+      }
+      res.setHeader('Content-Type','application/json');
+      res.end(JSON.stringify({"ok":true}));
+      //===========分割线===================
+    });
+  }else{
+    res.end('Now Found!');
+  }
+});
+server.listen(4000,()=>{
+  console.log('服务正在4000端口上启动!');
+});
